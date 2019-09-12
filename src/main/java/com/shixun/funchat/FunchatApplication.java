98cb73ab89bc242fabf66c58d5f551370dd1ab69
vGenGv <vgengv@outlook.com>
2019-09-12 12:02:57 +0800

    添加基础工程

---
+++ src/main/java/com/shixun/funchat/FunchatApplication.java
@@ -0,0 +1,13 @@
+package com.shixun.funchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunchatApplication.class, args);
+    }
+
+}
