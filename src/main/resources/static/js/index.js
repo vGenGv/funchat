5d02db4c62b8a21bd276a24bdb355408f5831839
qiaofeng <1270047253@qq.com>
2019-09-27 15:39:03 +0800

    前端显示群聊、删除群聊2

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,18 +5,58 @@
  */
 
 !$(function () {
-    var iNdex = {};
+    var iNdex = {
+        displayGroup: function () {
+            funChat.Utils.jsonAjax("/GroupControl", "post",
+                {func: "displayGroup"},
+                {
+                    success_call: function (map) {
+                        let list = [];
+                        for (var i = 0; i < map.return.length; i++) {
+                            var item = {};
+                            var o = map.return[i];
+                            item.title = o.gropName;
+                            item.description = "ID: " + o.gropId + ", 人数: " + o.sum;
+                            item.color = "success";
+                            item.icon = funChat.Utils.randomLetter(1, 26);
+                            item.downlist = [];
+                            item.downlist.push({text: "信息", value: "message"});
+                            item.downlist.push({text: "删除", value: "delete"});
+                            item.datalist = [];
+                            item.datalist.push({key: "group-id", value: o.gropId});
+                            item.datalist.push({key: "group-name", value: o.gropName});
+                            list.push(item);
+                        }
+                        funChat.List.updateList("chats", list);
+                    }
+                })
+        }
+    };
+
+    $(document).on("click", "#chats [data-list-dropdown='delete']", function () {
+        // 这个this指向当前点击对象,this是JS对象的一个特殊指针，它的指向根据环境不同而发生变化
+        var o = $(this).closest(".list-group-item");
+        var group_id = o.data("group-id");
+        //暂时我这么理解：获取当前点击的自定义属性（是jquery对象）并把它转化为JavaScript对象.
+
+        funChat.Utils.jsonAjax("/GroupControl", "post",
+            {func: "isOwner", groupId: group_id},
+            {
+                success_call: function (map) {
+                    o.remove();
+                    alert("删除groupId为" + group_id + "的群成功！");
+                },
+                failed_call: function (map) {
+                    alert("isOwner: false, 不是群主，没有权限！");
+                }
+            }
+        )
+    });
 
     //初始化
     $(document).ready(function () {
         funChat.Started.pageLoadingClose();
-        funChat.List.updateList("friends",
-            [{
-                title: funChat.Utils.randomLetter(1, 26),
-                description: "HaHaHa",
-                color: "warning",
-                datalist: [{key: "hahaha", value: "123"}, {key: "aaa", value: "456"}]
-            }]);
+        iNdex.displayGroup();
     });
 
 });
