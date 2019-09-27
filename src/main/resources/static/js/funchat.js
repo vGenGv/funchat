686d8e1cd470bdf769d0e029131fc9c1913b7107
vGenGv <vGenGv@outlook.com>
2019-09-27 15:38:45 +0800

    添加显示好友列表

--- src/main/resources/static/js/funchat.js
+++ src/main/resources/static/js/funchat.js
@@ -6,14 +6,16 @@
                 var o = $("#" + id + " .sidebar-body .list-group");
                 //清空列表
                 o.empty();
-                for (i = 0; i < list.length; i++) {
-                    o.append(this.listDom(list[i]));
+                var str = "";
+                for (var i = 0; i < list.length; i++) {
+                    str += this.listDom(list[i]);
                 }
+                o.append(str);
             },
             listDom: function (item) {
                 var str = '<li class="list-group-item"';
                 var datalist = item.datalist ? item.datalist : [];
-                for (i = 0; i < datalist.length; i++) {
+                for (var i = 0; i < datalist.length; i++) {
                     if (typeof (datalist[i].key) != "undefined")
                         str += ' data-' + datalist[i].key + '="' + datalist[i].value + '" ';
                 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,18 +5,72 @@
  */
 
 !$(function () {
-    var iNdex = {};
+    var iNdex = {
+        displayGroup: function () {
+            funChat.Utils.jsonAjax("/GroupControl", "post",
+                {func: "displayGroup"},
+                {
+                    success_call: function (map) {
+                        var list = [];
+                        for (var i = 0; i < map.return.length; i++) {
+                            var item = {};
+                            var o = map.return[i];
+                            item.title = o.gropName;
+                            item.description = "ID: " + o.gropId + ", 人数: " + o.sum;
+                            item.color = "success";
+                            item.icon = funChat.Utils.randomLetter(1, 26);
+                            item.downlist = [];
+                            item.downlist.push({text: "信息", value: "message"});
+                            item.downlist.push({text: "删除", value: "delete"});
+                            item.datalist = [];
+                            item.datalist.push({key: "group-id", value: o.gropId});
+                            item.datalist.push({key: "group-name", value: o.gropName});
+                            list.push(item);
+                        }
+                        funChat.List.updateList("chats", list);
+                    }
+                })
+        },
+        displayFriend:function () {
+            var list = [];
+            funChat.Utils.jsonAjax("/FriendControl", "post",
+                {func: "getUserFriendsAccept"},
+                {
+                    success_call: function (map) {
+                        for (var i = 0; i < map.return.length; i++) {
+                            var item = {};
+                            var o = map.return[i];
+                            item.title = o.username;
+                            item.description = "ID: " + o.id;
+                            item.color = "success";
+                            item.icon = funChat.Utils.randomLetter(1, 26);
+                            item.downlist = [];
+                            item.downlist.push({text: "资料", value: "message"});
+                            item.downlist.push({text: "删除", value: "delete"});
+                            item.datalist = [];
+                            item.datalist.push({key: "user-id", value: o.id});
+                            item.datalist.push({key: "user-name", value: o.username});
+                            item.datalist.push({key: "user-gender", value: o.geder});
+                            item.datalist.push({key: "user-tel", value: o.telephone});
+                            item.datalist.push({key: "user-email", value: o.mail});
+                            item.datalist.push({key: "user-addr", value: o.addr});
+                            item.datalist.push({key: "user-sign", value: o.perSignature});
+                            item.datalist.push({key: "user-birthday", value: o.birthday});
+                            item.datalist.push({key: "user-color", value: item.color});
+                            item.datalist.push({key: "user-icon", value: item.icon});
+                            list.push(item);
+                        }
+                        funChat.List.updateList("friends", list);
+                    }
+                })
+        }
+    };
 
     //初始化
     $(document).ready(function () {
         funChat.Started.pageLoadingClose();
-        funChat.List.updateList("friends",
-            [{
-                title: funChat.Utils.randomLetter(1, 26),
-                description: "HaHaHa",
-                color: "warning",
-                datalist: [{key: "hahaha", value: "123"}, {key: "aaa", value: "456"}]
-            }]);
+        iNdex.displayGroup();
+        iNdex.displayFriend();
     });
 
 });
